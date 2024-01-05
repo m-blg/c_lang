658ae465b2a5b5344023dc0101e4c462c37253bc
mbgl <caaalm3@gmail.com>
2024-01-05 14:20:30 +0300

    identifier parser

--- lib/c_lexer.py
+++ lib/c_lexer.py
@@ -1,4 +1,5 @@
 
+from math import inf
 from typing import TypeAlias, TypeVar, Optional, Callable
 import re
 
@@ -50,6 +51,7 @@ type Parser[PARSER_DATA_T, PARSER_TERM_T] = Callable[[
 
 # CLEXER_TERM_T = TypeVar('CLEXER_TERM_T', 'CToken', 'list[CToken]')
 type CLexer[CLEXER_TERM_T] = Parser[LexerData, CLEXER_TERM_T]
+type CLexerRet[CLEXER_TERM_T] = Optional(tuple[CLEXER_TERM_T, str, LexerData])
 
 
 class LexerData:
@@ -59,16 +61,27 @@ class LexerData:
     def __init__(self, line, col) -> None:
         self.line = line
         self.col = col
+        
+    def __hash__(self) -> int:
+        return hash((self.line, self.col))
+    
+    def __eq__(self, __value: object
+               ) -> bool:
+        return hash(self) == hash(__value)
 
 
 class CToken:
-    def __init__(self, type: str, parser: Optional[CLexer], text: Optional[str], line: Optional[int], col: Optional[int]) -> None:
+    def __init__(self, type: str, text: Optional[str], line: Optional[int], col: Optional[int]) -> None:
         self.type = type
-        self.parser = parser
         self.text = text
         self.line = line
         self.col = col
 
+def parse_end_of_input(s: str, data: LexerData) -> CLexerRet[list[CToken]]:
+    if len(s) == 0:
+        return ([], s, data)
+    else:
+        return None
 
 def skip_whitespace(s: str, data: LexerData) -> tuple[str, LexerData]:
     i, line, col = 0
@@ -83,7 +96,7 @@ def skip_whitespace(s: str, data: LexerData) -> tuple[str, LexerData]:
 
     return (s[i:], LexerData(data.line + line, data.col + col))
 
-def parse_whitespace(s: str, data: LexerData) -> tuple[list[CToken], str, LexerData] | None:
+def parse_whitespace(s: str, data: LexerData) -> CLexerRet[list[CToken]]:
     if len(s) == 0 or not s[0].isspace():
         return None
 
@@ -91,19 +104,41 @@ def parse_whitespace(s: str, data: LexerData) -> tuple[list[CToken], str, LexerD
         case None: return None
         case rest, next_data: return ([], rest, next_data)
 
-def parse_keyword(s: str, data: LexerData) -> tuple[CToken, str, LexerData] | None:
+# def parse_end_of_identifier(s: str, data: LexerData) -> CLexerRet:
+#     return combinator_choise([])
+
+def parse_identifier(s: str, data: LexerData) -> CLexerRet[CToken]:
+    m = re.match("[_a-zA-Z][_a-zA-Z0-9]*", s)
+    if m is None:
+        return None
+    token_text = m.group()
+    token = CToken(
+        type="identifier", 
+        text=token_text,
+        line=data.line,
+        col=data.col)
+    rest = s[len(token_text):]
+    rest_data = LexerData(data.line, data.col + len(token_text))
+    return token, rest, rest_data
+
+    
+
+def parse_keyword(s: str, data: LexerData) -> CLexerRet[CToken]:
+    col = data.col
     for t in C_KEYWORDS:
         if s.startswith(t):
             rest = s[len(t):]
-            m = re.match(r"\s+", rest)
-            if m:
+            col += len(t)
+            rest_data = LexerData(data.line, col)
+
+            res = parse_identifier(rest, rest_data)
+            if res is None:
                 return (CToken(type="keyword",
-                               parser=parse_keyword,
                                text=t,
                                line=data.line,
                                col=data.col),
                         rest,
-                        LexerData())
+                        rest_data)
 
     return None
 
@@ -115,7 +150,7 @@ def parse_keyword(s: str, data: LexerData) -> tuple[CToken, str, LexerData] | No
 
 def combinator_range[D, T](parser: Parser[D, T], min: int, max: int) -> Parser[D, list[T]]:
     # assert min <= max
-    def out_parser(s: str, data: D) -> Optional[tuple[list[T], str, D]]:
+    def out_parser(s: str, data: D) -> CLexerRet[list[T]]:
         tokens = []
         for _ in range(min):
             match parser(s, data):
@@ -146,22 +181,41 @@ def combinator_range[D, T](parser: Parser[D, T], min: int, max: int) -> Parser[D
 
 #     return out_parser
 
+# type ParserRet[D, T] = Result[tuple[T, str, D], Err[tuple[str, D]]] 
+# type Parser[D, T] = Callable[[str, D], ParserRet]
 
-def combinator_optional(parser: Parser) -> Parser:
+
+def combinator_optional[D, T](parser: Parser[D, T]) -> Parser[D, list[T]]:
     return combinator_range(parser, 0, 1)
 
+def combinator_many[D, T](parser: Parser[D, T]) -> Parser[D, list[T]]:
+    return combinator_range(parser, 0, inf)
 
-C_TOKENS = [
-    CToken(type="keyword",
-           parser=None,
-           text=None,
-           line=None,
-           col=None),
-    CToken(type="identifier",
-           #    re="[_a-zA-Z][_a-zA-Z0-9]*",
-           parser=None,
-           text=None,
-           line=None,
-           col=None),
+def combinator_some[D, T](parser: Parser[D, T]) -> Parser[D, list[T]]:
+    return combinator_range(parser, 1, inf)
 
-]
+def combinator_map[D, T, R](parser: Parser[D, T], fn: Callable[[T], R]) -> Parser[D, R]:
+    def out_parser(s: str, data: D) -> CLexerRet[R]:
+        match parser(s, data):
+            case None: return None
+            case res, rest, rest_data: return (fn(res), rest, rest_data)
+    
+    return out_parser
+
+C_TOKENS: dict[str, Parser] = {
+    "keyword": parse_keyword,
+    "identifier": parse_identifier,
+}
+
+# C_TOKENS = [
+#     CToken(type="keyword",
+#            text=None,
+#            line=None,
+#            col=None),
+#     CToken(type="identifier",
+#            #    re="[_a-zA-Z][_a-zA-Z0-9]*",
+#            text=None,
+#            line=None,
+#            col=None),
+
+# ]
---
+++ tests/test_c_lexer.py
@@ -0,0 +1,31 @@
+# import sys
+# sys.path.insert(1, './lib/')
+from ..lib.c_lexer import *
+
+
+def test_parse_identifier():
+    ld = LexerData(1, 1)
+    
+    tok, _, _ = parse_identifier("id", ld)
+    assert tok.text == "id"
+
+    tok, rest, rest_ld = parse_identifier("_id09 rest", ld)
+    assert tok.text == "_id09"
+    assert rest == " rest"
+    assert rest_ld == LexerData(1, len(tok.text)+1)
+
+def test_parse_keyword():
+    ld = LexerData(1, 1)
+    
+    tok, _, _ = parse_keyword("if", ld)
+    assert tok.text == "if"
+
+    assert parse_keyword("iff", ld) is None
+
+    tok, rest, rest_ld = parse_keyword("auto(rest", ld)
+    assert tok.text == "auto"
+    assert rest == "(rest"
+    assert rest_ld == LexerData(1, len(tok.text)+1)
+
+# def test_skip_whitespace():
+#     lex.parse_whitespace("   ", )
